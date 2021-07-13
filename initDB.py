26f8708ed00359a47d585710c0fffe5f2684f836
roshak7 <roshak7@gmail.com>
2021-07-13 15:36:59 +0300

    first

---
+++ initDB.py
@@ -0,0 +1,91 @@
+def init():
+    """
+    в shell:
+    from initDB import init
+    init()
+    """
+    from django.contrib.auth.models import User
+    from news.models import Author, Category, Post, PostCategory, Comment
+
+    print(f'DB initialization started...')
+
+    print(f' Очистка объектов...')
+    PostCategory.objects.all().delete()
+    print(f'  PostCategories = {PostCategory.objects.count()}')
+    Category.objects.all().delete()
+    print(f'  Categories = {Category.objects.count()}')
+    Author.objects.all().delete()
+    print(f'  Authors = {Author.objects.count()}')
+    User.objects.all().delete()
+    print(f'  Users = {User.objects.count()}')
+    print(f' Очистка объектов завершена')
+
+    print(f' Ввод новых данных...')
+    print(f'  Создание пользователей и авторов...')
+    User(username="user1").save()
+    User(username="user2").save()
+    User(username="comment_user1").save()
+    User(username="comment_user2").save()
+    print(f'   Users = {User.objects.count()}')
+
+    Author(user=User.objects.get(username="user1"), author_name="author1").save()
+    Author(user=User.objects.get(username="user2"), author_name="author2").save()
+    print(f'   Authors = {Author.objects.count()}')
+    print(f'  Пользователи и авторы созданы')
+
+    print(f'  Создание категорий...')
+    c1 = Category(name="cat1")
+    c1.save()
+    c2 = Category(name="cat2")
+    c2.save()
+    c3 = Category(name="cat3")
+    c3.save()
+    c4 = Category(name="cat4")
+    c4.save()
+    print(f'   Categories = {Category.objects.count()}')
+    print(f'  Категории созданы')
+
+    print(f'  Создание постов...')
+    print(f'   Пост 1...')
+    a = Author.objects.get(author_name='author1')
+    p = Post(post_type='0', author=a, post_header='PostHeader1', post_body='post_body1', rating=1)
+    p.save()
+    print(f'   Пост 1 создан')
+    print(f'   Пост 1. Присвоение категорий...')
+    PostCategory(post=p, category=c1).save()
+    PostCategory(post=p, category=c2).save()
+    print(f'   Пост 1. Категории присвоены')
+    print(f'   Пост 2...')
+    p = Post(post_type='1', author=a, post_header='PostHeader2', post_body='post_body2', rating=4)
+    p.save()
+    print(f'   Пост 2 создан')
+    print(f'   Пост 2. Присвоение категории...')
+    PostCategory(post=p, category=c3).save()
+    print(f'   Пост 2. Категория присвоена')
+    print(f'   Пост 3...')
+    a = Author.objects.get(author_name='author2')
+    p = Post(post_type='1', author=a, post_header='NewsHeader3', post_body='news_body1', rating=5)
+    p.save()
+    PostCategory(post=p, category=c4).save()
+    print(f'   Пост 3 создан')
+    print(f'   Posts = {Post.objects.count()}')
+    print(f'  Посты созданы')
+    print(f' Ввод новых данных завершён')
+
+    print(f'  Создание комментов...')
+    p1 = Post.objects.get(post_header='PostHeader1')
+    p2 = Post.objects.get(post_header='PostHeader2')
+    u1 = User.objects.get(username='comment_user1')
+    Comment(post=p1, user=u1, comment_text='Комментарий №1. Текст комментария', rating=1).save()
+    u2 = User.objects.get(username='comment_user2')
+    Comment(post=p1, user=u2, comment_text='Комментарий №2. Грамотно излагаешь! Уважаю!', rating=2).save()
+
+    Comment(post=p2, user=u1, comment_text='Комментарий №3. Текст комментария', rating=1).save()
+    p2 = Post.objects.get(post_header='NewsHeader3')
+    Comment(post=p2, user=u1, comment_text='Комментарий №4. 1 Комментарий к новости', rating=1).save()
+    Comment(post=p2, user=u2, comment_text='Комментарий №5. 2 Комментарий к новости', rating=2).save()
+
+    print(f'   Comments = {Comment.objects.count()}')
+    print(f'  Комментарии созданы')
+
+    print(f'DB initialization complete')
